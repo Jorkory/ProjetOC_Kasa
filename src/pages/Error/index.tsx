3fabae5e92bd96e8fe1794d0bfac667d07f0bb7d
Joris Lamaque <jor57lam@gmail.com>
2023-12-01 10:12:57 +0100

    Ajout: page d'error 404

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
+
 function Error() {
 
 
     return (
-        <>
-        Erreur 404
-        </>
+        <main>
+            <section className="error">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <Link to='/'>Retourner sur la page d'accueil</Link>
+            </section>
+        </main>
     )
 }
 
